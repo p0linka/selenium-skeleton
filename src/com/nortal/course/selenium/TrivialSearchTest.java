e8a2b0548cae6b65339c79738527577a5f3661a9
p0linka <mrubbishm@gmail.com>
2015-01-20 01:04:38 +0200

    Xpath

--- src/com/nortal/course/selenium/TrivialSearchTest.java
+++ src/com/nortal/course/selenium/TrivialSearchTest.java
@@ -28,27 +28,35 @@ public class TrivialSearchTest {
 
         searchFor("Nortal");
 
-        // verify something..
-        List<WebElement> nortalLinks =
-                driver.findElements(new ByChained(By.className("result-item"), By.partialLinkText("Nortal")));
+            // verify something..
+            List<WebElement> nortalLinks =
+                    //driver.findElements(new ByChained(By.className("result-item"), By.partialLinkText("Nortal")));
+                    driver.findElements(By.xpath("//ul[@class='results-type-2']//a[contains(.,'Nortal')]"));
+
+        for (WebElement nortalLink : nortalLinks) {
+            System.out.println(nortalLink.getAttribute("href"));
+        }
+
         Assert.assertTrue("there should be more than 1 link with 'Nortal' word", nortalLinks.size() > 1);
-        WebElement firstNortalLink = nortalLinks.get(0);
+            WebElement firstNortalLink = nortalLinks.get(0);
 
-        Assert.assertTrue("First link in search result should lead to 'www.nortal.ee'",
-                firstNortalLink.getAttribute("href").contains("www.nortal.ee"));
+            Assert.assertTrue("First link in search result should lead to 'www.nortal.ee'",
+                    firstNortalLink.getAttribute("href").contains("www.nortal.ee"));
 
-        firstNortalLink.click();
-        Assert.assertEquals("http://www.nortal.ee/", driver.getCurrentUrl());
+            firstNortalLink.click();
+            Assert.assertEquals("http://www.nortal.ee/", driver.getCurrentUrl());
 
-        // finalize stuff, closes driver/browser
-        driver.quit();
-    }
+            // finalize stuff, closes driver/browser
+            driver.quit();
+            }
 
     private void searchFor(String keyword) {
         WebElement queryInput =
-                driver.findElement(By.id("search-bar")).findElement(By.cssSelector("input.input[name='query']"));
+               // driver.findElement(By.id("search-bar")).findElement(By.cssSelector("input.input[name='query']"));
+                driver.findElement(By.xpath("//div[@id='search-bar']//input[@name='query']"));
         queryInput.sendKeys(keyword);
 
-        driver.findElement(By.cssSelector("form[action='/cgi-bin/otsing'] input.button[type='submit']")).click();
+        //driver.findElement(By.cssSelector("form[action='/cgi-bin/otsing'] input.button[type='submit']")).click();
+        driver.findElement(By.xpath("//form[@action='/cgi-bin/otsing']//input[@type='submit']")).click();
     }
 }
